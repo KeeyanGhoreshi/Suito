0f66cd9a0a6616e9b6c6c642e790dc872660d955
Keeyan <keeyan.ghoreshi@uconn.edu>
2017-08-28 16:10:39 -0400

    Delete sqlconnector.php

--- sqlconnector.php
+++
@@ -1,7 +0,0 @@
-<?php$user="root";$password="Qweasd123$";$database="stuff";
-mysql_connect(localhost,$user,$password);@mysql_select_db($database) or die(
-"Unable to select database");
-$query = "INSERT INTO grocery VALUES ('','$field1-name','$field2-name'
-,'$field3-name','$field4-name','$field5-name')";
-
-mysql_query($query);mysql_close();?>
